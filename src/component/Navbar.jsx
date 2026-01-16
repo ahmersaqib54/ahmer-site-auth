d84b65e0ae6a297cd6b7d31e788368de346222d7
ahmersaqib54 <ahmer.saqib.54@gmail.com>
2026-01-16 17:01:59 +0500

    authentication practice

---
+++ src/component/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+  <header className="bg-blue-900">
+    <nav className="mx-auto flex h-20px
+     w-auto items-center justify-between p-6 lg:px-8">
+    
+    </nav>
+  </header>
+    </>
+  )
+}
+
+export default Navbar
